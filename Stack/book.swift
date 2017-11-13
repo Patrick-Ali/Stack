efed3140ad314c9c39cc4e87ed1b05604e698d04
Patrick Ali <alip@uni.coventry.ac.uk>
2017-11-13 14:30:05 +0000

    Book class

--- Stack/book.swift
+++ Stack/book.swift
@@ -7,3 +7,54 @@
 //
 
 import Foundation
+
+struct Book {
+    var title: String
+    var author: String
+    var id: String
+    
+}
+
+struct BookDetails {
+    var title: String
+    var author: String
+    var description: String
+}
+
+enum JSONError: Error{
+    case InvalidURL(String)
+    case InvalidKey(String)
+    case InvalidArray
+    case InvalidData
+    case InvalidImage
+    case indexOutOfRange
+}
+
+class Books{
+    public static var sharedInstance = Books()
+    var searchData:[Book]
+    
+    private init (){
+        searchData = []
+    }
+    
+    public func getBook() throws -> Book{
+        return Book(title:"The Hobbit", author:"J.R.R. Tolkien", id:"abc")
+    }
+    
+    public var count:Int{
+        get{
+            return 0
+            //searchData.count
+        }
+    }
+    
+    public func search(withText text:String, _ completion: @escaping ()->())throws{
+        completion()
+        
+    }
+    
+    public func getDetails(withID id:String, _ completion: @escaping (BookDetails)->())throws {
+        completion(BookDetails(title:"The Hobbit", author:"J.R.R. Tolkien", description:"About a Hobbit"))
+    }
+}
